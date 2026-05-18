2fe371ebfe61df8fa012c7b9d76852a0a3919444
jihoon <wpwnshrck7@gmail.com>
2026-05-18 10:07:21 +0900

    Section 7: Add crown for winners and doesHighestScoreWin setting

--- ScoreKeeper/ScoreKeeper/ContentView.swift
+++ ScoreKeeper/ScoreKeeper/ContentView.swift
@@ -11,7 +11,10 @@ struct ContentView: View {
                 .bold()
                 .padding(.bottom)
             
-            SettingsView(startingPoints: $startingPoints)
+            SettingsView(
+                startingPoints: $startingPoints,
+                doesHighestScoreWin: $scoreboard.doesHighestScoreWin
+            )
 
             Grid {
                 GridRow {
@@ -23,7 +26,13 @@ struct ContentView: View {
 
                 ForEach($scoreboard.players) { $player in
                     GridRow {
-                        TextField("Name", text: $player.name)
+                        HStack {
+                            if scoreboard.winners.contains(player) {
+                                Image(systemName: "crown.fill")
+                                    .foregroundStyle(Color.yellow)
+                            }
+                            TextField("Name", text: $player.name)
+                        }
                         Text("\(player.score)")
                         Stepper("\(player.score)", value: $player.score)
                             .labelsHidden()
--- ScoreKeeper/ScoreKeeper/SettingsView.swift
+++ ScoreKeeper/ScoreKeeper/SettingsView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct SettingsView: View {
     @Binding var startingPoints: Int
+    @Binding var doesHighestScoreWin: Bool
 
     var body: some View {
         VStack(alignment: .leading) {
@@ -21,6 +22,11 @@ struct SettingsView: View {
                 Text("5 starting points").tag(5)
                 Text("10 starting points").tag(10)
             }
+
+            Picker("Winner", selection: $doesHighestScoreWin) {
+                Text("Highest score wins").tag(true)
+                Text("Lowest score wins").tag(false)
+            }
         }
         .padding()
         .background {
@@ -32,5 +38,6 @@ struct SettingsView: View {
 
 #Preview {
     @Previewable @State var startingPoints = 0
-    SettingsView(startingPoints: $startingPoints)
+    @Previewable @State var doesHighestScoreWin = true
+    SettingsView(startingPoints: $startingPoints, doesHighestScoreWin: $doesHighestScoreWin)
 }
